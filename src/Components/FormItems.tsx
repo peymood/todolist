280bc6804ad0ca4e5b0cf28ac31dff70c30402fb
peyman <pryman.edit123@gmail.com>
2024-08-07 10:57:32 +0330

    2nd

--- src/Components/FormItems.tsx
+++ src/Components/FormItems.tsx
@@ -1,56 +1,52 @@
-//list haye map shode task ha 
+//list haye map shode task ha
 
-
-import React, { useMemo, useState } from 'react';
-import Form from './Form';
-import { List, Button } from 'antd';
-import { DeleteOutlined } from '@ant-design/icons';
-import Search from './Search';
-import DeleteAll from './DeleteAll';
-
-
-
-
-
+import React, { useEffect, useMemo, useState } from "react";
+import Form from "./Form";
+import { List, Button } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
+import Search from "./Search";
+import DeleteAll from "./DeleteAll";
 
 const FormItems: React.FC = () => {
-  const [value, setValue] = useState<string>(''); 
-  const [task, setTask] = useState<string[]>([]); 
+  const [value, setValue] = useState<string>("");
+  const [task, setTask] = useState<string[]>([]);
 
-  const [search, setSearch] = useState<string>('') ; 
+  const [search, setSearch] = useState<string>("");
 
-  //delet kardan har task
+  //load krdn task ha
+  useEffect(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      setTask(JSON.parse(storedTasks));
+    }
+  }, []);
+
+  //update krdn task ha
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(task));
+  }, [task]);
+
+  //delete kardan har task
   const removeTask = (index: number) => {
     const updatedTasks = task.filter((_, i) => i !== index);
     setTask(updatedTasks);
-    setSearch('')
+    setSearch("");
   };
-
 
   const filterSearch = useMemo(() => {
     if (!search.trim()) return task;
-    return task.filter((t) =>
-      t.toLowerCase().includes(search.toLowerCase())
-    );
-  }, [search, task]) ;    
-    
-  
+    return task.filter((t) => t.toLowerCase().includes(search.toLowerCase()));
+  }, [search, task]);
 
   return (
-
     <div className="form-item">
-      
-      
-        <Search FormProps={{ task, value }} setSearch={setSearch} />
-        <Form task={task} value={value} setValue ={setValue} setTask={setTask}/>
-    
-      
+      <Search FormProps={{ task, value }} setSearch={setSearch} />
+      <Form task={task} value={value} setValue={setValue} setTask={setTask} />
 
-      
       <List
         size="small"
         bordered
-        dataSource={filterSearch} 
+        dataSource={filterSearch}
         renderItem={(item, index) => (
           <List.Item
             actions={[
@@ -66,12 +62,9 @@ const FormItems: React.FC = () => {
         )}
         style={{ marginTop: 10, width: 350 }}
       />
-      
-        <DeleteAll FormProps={{ task, value }} setTask={setTask} />
-      
-        
+
+      <DeleteAll FormProps={{ task, value }} setTask={setTask} />
     </div>
-   
   );
 };
 
